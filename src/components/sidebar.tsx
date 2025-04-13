56faf4494aef7efd73a77caddfea2184155190fd
osama <osamaqat24@gmail.com>
2025-04-13 13:48:19 +0300

    first commit

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Projects } from "./projects";
+import { Navigation } from "./navigation";
+import { DottedSeparator } from "./dotted-separator";
+import { WorkspaceSwitcher } from "./workspace-switcher";
+
+export const Sidebar = () => {
+  return (
+    <aside className="h-full bg-neutral-100 p-4 w-full">
+      <Link href="/">
+        {/* <Image src="/logo.svg" alt="logo" width={164} height={48} /> */}
+        <span className="text-2xl font-semibold text-blue-600">Quality Tasks</span>
+      </Link>
+      <DottedSeparator className="my-4" />
+      <WorkspaceSwitcher />
+      <DottedSeparator className="my-4" />
+      <Navigation />
+      <DottedSeparator className="my-4" />
+      <Projects />
+    </aside>
+  );
+};
